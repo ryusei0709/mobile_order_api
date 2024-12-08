d2a14a01044655e2584e9d4198bf0f5985babda5
tsuchiya <tsuchiya@ook-Pro.local>
2024-12-08 23:51:19 +0900

    gql init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
 
   # Defines the root path route ("/")
   # root "posts#index"
